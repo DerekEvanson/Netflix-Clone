1d6b8d89c1aa7b030eb8d4f206433447e00ce44b
Derek Evanson <evanson.derek@gmail.com>
2020-10-04 02:59:01 -0500

    Pulling Images

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 
+const base_url = 'https://image.tmdb.org/t/p/original';
+
 function Row({ title, fetchUrl }) {
 	const [movies, setMovies] = useState([]);
 
@@ -9,17 +11,28 @@ function Row({ title, fetchUrl }) {
 		// if brackets blank, run once at load
 		async function fetchData() {
 			const request = await axios.get(fetchUrl);
-			console.log(request);
+			setMovies(request.data.results);
 			return request;
 		}
 		fetchData();
-	}, []);
+	}, [fetchUrl]);
 
 	return (
-		<div>
+		<div className="row">
 			<h2>{title}</h2>
 
-			{/* container -> posters */}
+			<div className="row__posters">
+				{/* several row__poster(s) */}
+
+				{movies.map((movie) => (
+					<img
+						className="row__poster"
+						src={base_url + movie.poster_path}
+						alt={movie.name}
+						key={movies.indexOf(movie)}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
