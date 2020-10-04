864a7f9a1cab9473e621e8d9fad47c72baada869
Derek Evanson <evanson.derek@gmail.com>
2020-10-04 15:13:32 -0500

    Banner Test CSS

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
 import requests from './requests';
+import './Banner.css';
 
 function Banner() {
 	const [movie, setMovie] = useState([]);
@@ -18,6 +19,10 @@ function Banner() {
 		fetchData();
 	}, []);
 
+	function truncate(str, n) {
+		return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+	}
+
 	return (
 		<header
 			className="banner"
@@ -28,9 +33,16 @@ function Banner() {
 			}}
 		>
 			<div className="banner__contents">
-				<h1>{movie?.title || movie?.name || movie?.original_name}</h1>
-				{/** div 2 btns */}
-				{/** description */}
+				<h1 className="banner__title">
+					{movie?.title || movie?.name || movie?.original_name}
+				</h1>
+				<div className="banner__buttons">
+					<button className="banner__button">Play</button>
+					<button className="banner__button">MyList</button>
+				</div>
+				<h1 className="banner__description">
+					{truncate(movie?.overview, 150)}
+				</h1>
 			</div>
 		</header>
 	);
