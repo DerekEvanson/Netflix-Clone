89a90142a6d16057ccb0fcfe4eb9367ccb7a5019
Derek Evanson <evanson.derek@gmail.com>
2020-10-04 12:40:02 -0500

    Large Row CSS

--- src/Row.js
+++ src/Row.js
@@ -4,32 +4,30 @@ import './Row.css';
 
 const base_url = 'https://image.tmdb.org/t/p/original';
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
 	const [movies, setMovies] = useState([]);
 
-	// A snippet of code which runs based on a specific condition
 	useEffect(() => {
-		// if brackets blank, run once at load
 		async function fetchData() {
 			const request = await axios.get(fetchUrl);
 			setMovies(request.data.results);
 			return request;
 		}
 		fetchData();
-	}, [fetchUrl]);
+	}, [fetchUrl, isLargeRow]);
 
 	return (
 		<div className="row">
 			<h2>{title}</h2>
 
 			<div className="row__posters">
-				{/* several row__poster(s) */}
-
 				{movies.map((movie) => (
 					<img
 						key={movie.id}
-						className="row__poster"
-						src={base_url + movie.poster_path}
+						className={'row__poster' + (isLargeRow ? ' row__poster_large' : '')}
+						src={
+							base_url + (isLargeRow ? movie.poster_path : movie.backdrop_path)
+						}
 						alt={movie.name}
 					/>
 				))}
